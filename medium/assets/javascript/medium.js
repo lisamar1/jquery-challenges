ef0634cbbeaa258b5a010fc63027ff90985ce9cd
Lisa Forrest <lisam.forrest@libertymutual.com>
2017-12-19 08:52:29 -0500

    jquery-challenge assignment

--- medium/assets/javascript/medium.js
+++ medium/assets/javascript/medium.js
@@ -36,14 +36,61 @@
  *   and how to use it in JS. You will also need to download a sound bite
  */
 
-(function(){
+$(function () {
 
-  //jQuery equivelent to window.onload = function{}
-  //code in here wont run until page loads
-  $(function(){
+  function randomNumber(lowerBound = 0, upperBound = 3) {
+    return Math.floor(Math.random() * (upperBound - lowerBound) + lowerBound);
+  }
 
+  function incrementCounter(counter) {
+    counter.html(parseInt(counterOne.html()) + 1);
+  }
+  const numResets = $("#num-resets");
 
+  $("button#reset").click(function(){
+   teamOneScore.html(0);
+   counterOne.html(0);
+   teamTwoScore.html(0);
+   counterTwo.html(0);
+   numResets.html(parseInt(numResets.html()) + 1);
+  })  
 
+  const incrementShootButtonTeamOne = $("#teamone-shoot");
+  const counterOne = $("#teamone-numshots");
+  const teamOneScore = $('#teamone-numhits');
+
+  incrementShootButtonTeamOne.click(function () {
+    const pointValueOfShot = randomNumber();
+
+    incrementCounter(counterOne);
+
+    if (pointValueOfShot > 0) { // scored 1-3
+      teamOneScore.html(parseInt(teamOneScore.html()) + pointValueOfShot);
+      $("body").css("background-color", "blue");  
+    } else { // scored 0
+      // change color of something
+    }
+
+    
   })
 
-})();
+  const incrementShootButtonTeamTwo = $("#teamtwo-shoot");
+  const counterTwo = $("#teamtwo-numshots");
+  const teamTwoScore = $('#teamtwo-numhits');
+
+  incrementShootButtonTeamTwo.click(function () {
+    const pointValueOfShot = randomNumber();
+
+    incrementCounter(counterTwo);
+
+    if (pointValueOfShot > 0) { // scored 1-3
+      teamTwoScore.html(parseInt(teamTwoScore.html()) + pointValueOfShot);
+      $("body").css("background-color", "red");
+    } else { // scored 0
+      // change color of something
+    }
+   
+  })
+  
+    
+  })
